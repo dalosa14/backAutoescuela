f5d5075f9c29914b54cfc16f0b94170190d40f03
saul <sagar5554@gmail.com>
2021-06-09 02:09:19 +0200

    postgres

--- dist/db/config.js
+++ dist/db/config.js
@@ -1,10 +1,10 @@
 "use strict";
 
 var database = {
-  username: process.env.DB_USERNAME || 'bcd1333518890c',
-  password: process.env.DB_PASSWORD || 'd81a913a',
-  database: process.env.DB_DATABASE || 'heroku_7bd13087cde2af2',
-  host: process.env.DB_HOST || 'us-cdbr-east-04.cleardb.com'
+  username:  'bcd1333518890c',
+  password:  'd81a913a',
+  database:  'heroku_7bd13087cde2af2',
+  host:  'us-cdbr-east-04.cleardb.com'
 };
 // mysql://bcd1333518890c:@/heroku_7bd13087cde2af2?reconnect=true
 module.exports = {
